5a0fad8061ecf7b3f66e6044fa3c266a72e0f388
Luckyalexandr <luckyalexandr@gmail.com>
2022-07-29 17:12:44 +0300

    Admin single category page

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -8,7 +8,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Категории</h1>
+                    <h1 class="m-0">{{ $category->title }}</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
@@ -26,44 +26,19 @@
         <div class="container-fluid">
             <!-- Small boxes (Stat box) -->
             <div class="row">
-                <div class="col-2 mb-3">
-                    <a href="{{ route('admin.category.create') }}" class="btn btn-block btn-primary">Добавить</a>
-                </div>
                 <div class="col-12">
                     <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Responsive Hover Table</h3>
-
-                            <div class="card-tools">
-                                <div class="input-group input-group-sm" style="width: 150px;">
-                                    <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-
-                                    <div class="input-group-append">
-                                        <button type="submit" class="btn btn-default">
-                                            <i class="fas fa-search"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- /.card-header -->
                         <div class="card-body table-responsive p-0">
                             <table class="table table-hover text-nowrap">
-                                <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Название</th>
-                                    <th>Действия</th>
-                                </tr>
-                                </thead>
                                 <tbody>
-                                @foreach($categories as $category)
                                 <tr>
+                                    <td>ID</td>
                                     <td>{{ $category->id }}</td>
-                                    <td>{{ $category->title }}</td>
-                                    <td><a href="{{ route('admin.category.show', $category->id) }}"><i class="far fa-eye"></i></a></td>
                                 </tr>
-                                @endforeach
+                                <tr>
+                                    <td>Название</td>
+                                    <td>{{ $category->title }}</td>
+                                </tr>
                                 </tbody>
                             </table>
                         </div>
