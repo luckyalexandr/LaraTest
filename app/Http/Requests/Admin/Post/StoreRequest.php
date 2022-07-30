0bdfb85bf007e8090062745bcae55f338c71a301
Luckyalexandr <luckyalexandr@gmail.com>
2022-07-30 16:05:35 +0300

    fix post store request

--- app/Http/Requests/Admin/Post/StoreRequest.php
+++ app/Http/Requests/Admin/Post/StoreRequest.php
@@ -26,8 +26,8 @@ class StoreRequest extends FormRequest
         return [
             'title' => 'required|string',
             'content' => 'required|string',
-            'preview_image' => 'required|file',
-            'main_image' => 'required|file',
+            'preview_image' => 'image',
+            'main_image' => 'image',
         ];
     }
 }
